a5abead16298ae904f989ec1e4654940d0a2f2a1
joel97vil <joel97vil@gmail.com>
2023-12-12 21:49:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,46 +23,27 @@ function loadingSpinner(options){
     //EVENTS
     const event = new Event("isFinished");
 
-
-    //VARIABLES
-    let _this;
-
-    let _overlay;
-    let _text;
-    let _secondary_text;
-    let _foreground_color;
-    let _background_color;
-    let _speed; //as millisecs
-    let _size;
-
-
     //INITIALIZATION
     options = { _foreground_color: DEFAULT_FOREGROUND, _overlay: false, _text: DEFAULT_TEXT, _speed: DEFAULT_SPEED, _background_color: DEFAULT_BACKGROUND, _secondary_text: null};
-    _foreground_color = options._foreground_color;
-    _overlay = options._overlay;
-    _text = options._text;
-    _speed = options._speed;
-    _background_color = options._background_color;
-    _secondary_text = options._secondary_text;
     _this = this;
 
 
     //ACCESSORS
     this.getInstance = function(){ return _this; }
-    this.getOverlay = function(){ return _overlay; }
-    this.setOverlay = function(overlay) { _overlay = overlay; }
-    this.getForegroundColor = function(){ return _foreground_color; }
-    this.setForegroundColor = function(foregroundColor){ _foreground_color = foregroundColor; }
-    this.getBackgroundColor = function(){ return _background_color; }
-    this.setBackgroundColor = function(backgorundColor){ _background_color = backgorundColor; }
-    this.getSpeed = function(){ return _speed; }
-    this.setSpeed = function(speed){ _speed = speed; }
-    this.getText = function(){ return _text; }
-    this.setText = function(text) { _text = text; }
-    this.getSecondaryText = function(){ return _secondary_text; }
-    this.setSecondaryText = function(secondary_text) { _secondary_text = secondary_text; }
-    this.getSize = function(){ return _size; }
-    this.setSize = function(size){ _size = size; }
+    this.getOverlay = function(){ return options._overlay; }
+    this.setOverlay = function(overlay) { options._overlay = overlay; }
+    this.getForegroundColor = function(){ return options._foreground_color; }
+    this.setForegroundColor = function(foregroundColor){ options._foreground_color = foregroundColor; }
+    this.getBackgroundColor = function(){ return options._background_color; }
+    this.setBackgroundColor = function(backgroundColor){ options._background_color = backgroundColor; }
+    this.getSpeed = function(){ return options._speed; }
+    this.setSpeed = function(speed){ options._speed = speed; }
+    this.getText = function(){ return options._text; }
+    this.setText = function(text) { options._text = text; }
+    this.getSecondaryText = function(){ return options._secondary_text; }
+    this.setSecondaryText = function(secondary_text) { options._secondary_text = secondary_text; }
+    this.getSize = function(){ return options._size; }
+    this.setSize = function(size){ options._size = size; }
 
 
     //FUNCTIONS
@@ -105,13 +86,13 @@ function loadingSpinner(options){
     this.init();
 
     // Listen the event.
-    /*this.addEventListener(
-        "isFinished",
+    window.addEventListener(
+        "load",
             (e, _this) => {
-                _this.stop();
+                /*_this.*/stop();
             },
         false,
-    );*/
+    );
 
     // Dispatch the event.
     //this.dispatchEvent(event);
